05baf927a74a05ea3e4227cba66986f0d5467bf7
Kyle <kemvy4@mst.edu>
2021-09-06 00:06:59 -0500

    added the ability to subtract values from a selected category
    added categorys current value on the update page

--- app/src/main/java/com/example/budgetapp/Fragments/SecondFragment.java
+++ app/src/main/java/com/example/budgetapp/Fragments/SecondFragment.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
@@ -47,6 +48,10 @@ import java.util.logging.Logger;
 public class SecondFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     private BudgetRequestViewModel requestViewModel;
+    static final String INPUT_TYPE_ADDITION = "INPUT_TYPE_ADDITION";
+    static final String INPUT_TYPE_SUBTRACTION = "INPUT_TYPE_SUBTRACTION";
+    TextView currentCategoryValueView = null;
+
 
     @Override
     public View onCreateView(
@@ -63,8 +68,12 @@ public class SecondFragment extends Fragment implements AdapterView.OnItemSelect
         //get view model
         requestViewModel = new ViewModelProvider(requireActivity()).get(BudgetRequestViewModel.class);
 
-         view.findViewById(R.id.submit_button).setVisibility(View.GONE);
+        //hide the value inputs on load
+         view.findViewById(R.id.add_button).setVisibility(View.GONE);
+         view.findViewById(R.id.subtract_button).setVisibility(View.GONE);
          view.findViewById(R.id.purchase_input).setVisibility(View.GONE);
+         view.findViewById(R.id.change_category_value_title).setVisibility(View.GONE);
+         view.findViewById(R.id.category_current_value_card).setVisibility(View.GONE);
 
 
         try {
@@ -105,84 +114,132 @@ public class SecondFragment extends Fragment implements AdapterView.OnItemSelect
     public void onItemSelected(AdapterView<?> parent, View view,
                                int pos, long id) {
 
-        Button submitButton = getView().findViewById(R.id.submit_button);
+        Button submitButton = getView().findViewById(R.id.add_button);
+        Button subtractButton = getView().findViewById(R.id.subtract_button);
         EditText purchaseInput = getView().findViewById(R.id.purchase_input);
+        TextView changeCategoryValueTitle = getView().findViewById(R.id.change_category_value_title);
+        //check if the default spinner option is not selected
         if(!parent.getItemAtPosition(pos).toString().equals("Select Budget Category")) {
 
-
+            //Set the current category page pieces to visible
             submitButton.setVisibility(View.VISIBLE);
+            subtractButton.setVisibility(View.VISIBLE);
             purchaseInput.setVisibility(View.VISIBLE);
+            changeCategoryValueTitle.setVisibility(View.VISIBLE);
+            getView().findViewById(R.id.category_current_value_card).setVisibility(View.VISIBLE);
 
 
+            //Create map to pull categories by spinner value name
             HashMap<String, BudgetCategory> categoryMap = new HashMap<>();
 
             requestViewModel.getBudgetSheet().getCategories().forEach(cat -> {
                 categoryMap.put(cat.getName(), cat);
             });
 
+            //retrieve selected category from spinner by spinner value
             BudgetCategory selectedCategory = categoryMap.get(parent.getItemAtPosition(pos).toString());
+
+            //Populate UI card with selected category information
+            TextView currentCategoryTitle = getView().findViewById(R.id.category_current_value_title);
+            currentCategoryTitle.setText(currentCategoryTitle.getText() + selectedCategory.getName() + ":");
+
+            currentCategoryValueView = getView().findViewById(R.id.category_current_value);
+
+
+            //retrieve selected category's value
+            final Number[] currentCategoryValue = {0};
+            String cellAddressToEdit = selectedCategory.getCategoryMonthCell(LocalDate.now().getMonth().name());
+
+            AsyncTask.execute(() -> {
+                try{
+                    ValueRange response = requestViewModel.getSheetService().spreadsheets().values()
+                            .get(requestViewModel.getBudgetSheet().getSpreadSheetId(), cellAddressToEdit)
+                            .execute();
+
+                    String displayCellValue = response.getValues() != null ? response.getValues().get(0).get(0).toString() : "$0.00";
+                    currentCategoryValueView.setText(displayCellValue);
+
+                    currentCategoryValue[0] = NumberFormat.getCurrencyInstance(Locale.US)
+                            .parse(displayCellValue);
+
+
+
+                } catch (IOException | ParseException e) {
+                    e.printStackTrace();
+                    Snackbar.make(getView(), "Failed to retrieve the selected cell's data, try refreshing the page" , Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+
+
 
 
             submitButton.setOnClickListener(l -> {
-                String cellToEdit = selectedCategory.getCategoryMonthCell(LocalDate.now().getMonth().name());
+                updateCellWithNewValue(cellAddressToEdit, purchaseInput.getText().toString(), currentCategoryValue[0].floatValue(), INPUT_TYPE_ADDITION);
+                purchaseInput.getText().clear();
+            });
 
-                updateCategoryMonthCell(cellToEdit, purchaseInput.getText().toString());
+            subtractButton.setOnClickListener(l -> {
+                updateCellWithNewValue(cellAddressToEdit, purchaseInput.getText().toString(), currentCategoryValue[0].floatValue(), INPUT_TYPE_SUBTRACTION);
                 purchaseInput.getText().clear();
             });
         } else {
             submitButton.setVisibility(View.GONE);
+            subtractButton.setVisibility(View.GONE);
             purchaseInput.setVisibility(View.GONE);
         }
 
 
     }
 
-    private void updateCategoryMonthCell(String cellAddressToRetrieve, String purchaseAmountToAdd) {
-        final Number[] decimalCellValue = {0};
-        AsyncTask.execute(() -> {
-            try{
-                //get current month
+//    private Number getCurrentCategoryValue(String cellAddressToRetrieve, String amountToUpdate, String inputType) {
+//        final Number[] decimalCellValue = {0};
+//        AsyncTask.execute(() -> {
+//            try{
+//                //get current month
+//
+//                ValueRange response = requestViewModel.getSheetService().spreadsheets().values()
+//                        .get(requestViewModel.getBudgetSheet().getSpreadSheetId(), cellAddressToRetrieve)
+//                        .execute();
+//
+//
+//                String displayCellValue = response.getValues() != null ? response.getValues().get(0).get(0).toString() : "$0.00";
+//                decimalCellValue[0] = NumberFormat.getCurrencyInstance(Locale.US)
+//                        .parse(displayCellValue);
+//
+//
+//            } catch (IOException | ParseException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//    }
 
-                ValueRange response = requestViewModel.getSheetService().spreadsheets().values()
-                        .get(requestViewModel.getBudgetSheet().getSpreadSheetId(), cellAddressToRetrieve)
-                        .execute();
-
-
-                String displayCellValue = response.getValues() != null ? response.getValues().get(0).get(0).toString() : "$0.00";
-                decimalCellValue[0] = NumberFormat.getCurrencyInstance(Locale.US)
-                        .parse(displayCellValue);
-
-                updateCellWithNewValue(cellAddressToRetrieve,  purchaseAmountToAdd, decimalCellValue[0].floatValue());
-
-
-            } catch (IOException | ParseException e) {
-                e.printStackTrace();
-            }
-        });
-  ;
-    }
-
-    private void updateCellWithNewValue(String cellToEdit, String purchaseAmountToAdd, Float currentCellValue) {
+    private void updateCellWithNewValue(String cellToEdit, String amountToUpdate, Float currentCellValue, String inputType) {
 
 
         List<List<Object>> values = Arrays.asList(
                 Arrays.asList(
-                        currentCellValue + Float.parseFloat(purchaseAmountToAdd)
+                        inputType.equals(INPUT_TYPE_ADDITION) ? currentCellValue + Float.parseFloat(amountToUpdate) : currentCellValue - Float.parseFloat(amountToUpdate)
                 )
         );
-        ValueRange newCellValue = new ValueRange().setValues(values);
+        ValueRange newCellValueRange = new ValueRange().setValues(values);
+        String newCellValue = NumberFormat.getCurrencyInstance(Locale.US).format(new BigDecimal(values.get(0).get(0).toString()));
+
         AsyncTask.execute(() -> {
             try {
                 UpdateValuesResponse result =
                         requestViewModel.getSheetService().spreadsheets().values().update(
-                                requestViewModel.getBudgetSheet().getSpreadSheetId(), cellToEdit, newCellValue)
+                                requestViewModel.getBudgetSheet().getSpreadSheetId(), cellToEdit, newCellValueRange)
                                 .setValueInputOption("USER_ENTERED")
                                 .execute();
                 if(result == null || result.getUpdatedCells() == 0){
                     Logger.getLogger("No cells updated");
+                    Snackbar.make(getView(), "Failed to update the selected cell's data, please try again" , Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
 
                 } else {
-                    Snackbar.make(getView(), "Update successful, new value: " + newCellValue.getValues().get(0).get(0) , Snackbar.LENGTH_LONG)
+                    currentCategoryValueView.setText(newCellValue);
+                    Snackbar.make(getView(), "Update successful, new value: " + newCellValue, Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
                     Logger.getLogger("Successfully updated " + result.getUpdatedCells() + " cells");
 
